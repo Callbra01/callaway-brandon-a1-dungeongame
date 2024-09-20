db1b50f19d96d996441ede04667d8a5409f96038
wightkid <103604852+wightkid@users.noreply.github.com>
2024-09-20 11:47:44 -0400

    Add last opponent var Finish first attack sequence Add next room with left or right choice

--- callaway-brandon-a1-textbasedgame/Program.cs
+++ callaway-brandon-a1-textbasedgame/Program.cs
@@ -2,11 +2,15 @@
 string playerInput = "";
 string playerName = "";
 string playerClass = "";
-string opponentName = "";
 int playerDMG = 0;
 int playerHP = 100;
-int opponentHP = 0;
+int playerGold = 0;
 bool playerCanFlee = false;
+
+// Initialize and Declare opponent vars
+string opponentName = "";
+int opponentHP = 0;
+int opponentDMG = 0;
 
 // Initialize and Declare class-specific vars
 int warriorDMG = 35;
@@ -18,7 +22,6 @@ int wretchHP = 85;
 
 // Initialize and Declare room tracking var
 int roomCount = 0;
-
 
 // Ask player for name and get input
 Console.WriteLine("enter name:");
@@ -64,9 +67,11 @@ else
 // First room/scene
 if (roomCount == 1)
 {
-    // Give opponent hp value and name, inform player of playername and class choice
+    // Give opponent hp value, damage value, and name, inform player of playername and class choice
     opponentHP = 60;
+    opponentDMG = 25;
     opponentName = "ghoul";
+    
     Console.WriteLine($"{playerName}, the {playerClass}...\ntype anything to continue...");
     Console.ReadLine();
     Console.Clear();
@@ -76,6 +81,8 @@ if (roomCount == 1)
     Console.WriteLine("A ghoul blocks the way, what will you do: attack, flee, or sacrifice?");
     playerInput = Console.ReadLine();
 
+    // if player attacks, inform player of relevant information
+    // If opponent is still alive, attack the player again
     if (playerInput == "attack")
     {
         opponentHP -= playerDMG;
@@ -83,6 +90,32 @@ if (roomCount == 1)
         if (opponentHP > 0)
         {
             Console.WriteLine($"{opponentName} has {opponentHP}hp left!");
+            playerHP -= opponentDMG;
+            Console.WriteLine($"\n{opponentName} strikes, dealing {opponentDMG} damage!!!");
+            opponentHP -= playerDMG;
+            Console.WriteLine($"{playerName} the {playerClass} attacks the {opponentName}, dealing {playerDMG} damage!");
+            if ( opponentHP <= 0)
+            {
+                roomCount = 2;
+                playerGold += 25;
+                Console.WriteLine($"\n{playerName} the {playerClass} defeated the {opponentName}!!");
+                Console.WriteLine($"\n {playerName} now has {playerGold} gold, and {playerHP}hp!");
+                Console.WriteLine("type anything to continue");
+                Console.ReadLine();
+            }
+        }
+        else
+        {
+            // This line is for wretches, as their damage one shots enemies
+            if (opponentHP <= 0)
+            {
+                roomCount = 2;
+                playerGold += 25;
+                Console.WriteLine($"{playerName} the {playerClass} defeated the {opponentName}!!");
+                Console.WriteLine($"\n{playerName} now has {playerGold} gold, and {playerHP}hp...");
+                Console.WriteLine("type anything to continue");
+                Console.ReadLine();
+            }
         }
     }
     // If player selects flee, and canFlee is true, allow the player to continue with no gold gain
@@ -101,6 +134,7 @@ if (roomCount == 1)
             roomCount = 666;
         }
     }
+    // If player input is invalid, end the game
     else
     {
         Console.WriteLine($"{playerName}'s foolishness has rendered their life forfiet.");
@@ -108,6 +142,39 @@ if (roomCount == 1)
     }
 }
 
+if (roomCount == 2)
+{
+    Console.Clear();
+    Console.WriteLine("You have managed to make it to the next room");
+    Console.WriteLine("You see a door to your left and a hole in the wall to your right.");
+    Console.WriteLine("Which direction do you choose? \"left\" or \"right\"");
+    playerInput = Console.ReadLine();
+
+    if (playerInput == "left")
+    {
+        roomCount = 3;
+    }
+    else if (playerInput == "right")
+    {
+        roomCount = 4;
+    }
+    else
+    {
+        roomCount = 666;
+    }
+}// Player chose the door to their left in the last room
+if (roomCount == 3)
+{
+    Console.Clear();
+    Console.WriteLine("NEXT ROOM");
+}
+
+// Player chose the hole in the wall to their right in the last room
+// This room contains a chest
+if (roomCount == 4)
+{
+    Console.Clear();
+}
 
 // Failure room, ends game
 if (roomCount == 666)
