61e017c6f49b79c50b7a5e2eb45f273adc8ae2fa
wightkid <103604852+wightkid@users.noreply.github.com>
2024-09-20 00:32:14 -0400

    Add player variables Add room tracking variable
    Ask player for name
    Ask player for class
    Add class choice if statement
    Subsequently add failure room

--- callaway-brandon-a1-textbasedgame/Program.cs
+++ callaway-brandon-a1-textbasedgame/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Initialize and Declare player vars
+string playerInput = "";
+string playerName = "";
+string playerClass = "";
+int playerDMG;
+int playerHP;
+int opponentHP;
+int warriorDMG = 35;
+int warriorHP = 150;
+int wizardDMG = warriorDMG;
+int wizardHP = 120;
+int wretchDMG = 65;
+int wretchHP = 85;
+
+// Initialize and Declare room tracking var
+int roomCount = 0;
+
+
+// Ask player for name and get input
+Console.WriteLine("enter name:");
+playerName = Console.ReadLine();
+
+// Tell player class information and ask to choose
+Console.WriteLine("in this land, warriors: wizards: wretchs");
+Console.WriteLine("pick");
+playerInput = Console.ReadLine();
+
+// If player chooses a valid class, set playerVars to corresponding classVars.
+// Else if player types an invalid choice, set roomCount to 0 (resulting in ending the game)
+if (playerInput == "warrior")
+{
+    playerDMG = warriorDMG;
+    playerHP = warriorHP;
+
+    roomCount = 1;
+}
+else if (playerInput == "wizard")
+{
+    playerDMG = wizardDMG;
+    playerHP = wizardHP;
+
+    roomCount = 1;
+}
+else if (playerInput == "wretch")
+{
+    playerDMG = wretchDMG;
+    playerHP = wretchHP;
+
+    roomCount = 1;
+}
+else
+{
+    roomCount = 666;
+}
+
+// First room/scene
+if (roomCount == 1)
+{
+    //TODO: print intro, delve into first attack sequences
+}
+
+
+// Failure room, ends game
+if (roomCount == 666)
+{
+    Console.WriteLine("failure");
+    Console.ReadLine();
+}
