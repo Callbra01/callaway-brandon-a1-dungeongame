2154731c9db9ae33aef07a0216a8e29f927241d6
wightkid <103604852+wightkid@users.noreply.github.com>
2024-09-23 15:12:33 -0400

    Change various text / prompts Add boss room mechanic Remove flee option from boss room Add playerSelectionInvalid bool for invalid text check in room 666

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # callaway-brandon-a1-dungeongame
 Brandon Callaway's Dungeon Crawler (esque!) project. This project is for my semester 1 Game Development Course.
+
+Instructions:
+To play this game, you will be prompted to answer with one of the provided words, the decisions are upto you.
--- callaway-brandon-a1-textbasedgame/Program.cs
+++ callaway-brandon-a1-textbasedgame/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Declare and initialize player vars
-string playerInput = "";
-string playerName = "";
+string playerInput;
+string playerName;
 string playerClass = "";
 int playerDMG = 0;
 int playerHP = 100;
@@ -9,20 +9,21 @@ bool playerCanFlee = false;
 bool playerSelectionInvalid = false;
 
 // Declare and initialize opponent vars
-string opponentName = "";
-int opponentHP = 0;
-int opponentDMG = 0;
+string opponentName;
+int opponentHP;
+int opponentDMG;
 
-// Declare and initialize class-specific vars
+// Declare and initialize class-specific vars, wizards and warriors have the same hp
 int warriorDMG = 35;
-int warriorHP = 150;
+int warriorHP = 110;
 int wizardDMG = warriorDMG;
-int wizardHP = 120;
+int wizardHP = 100;
 int wretchDMG = 65;
-int wretchHP = 85;
+int wretchHP = 65;
+string classBoss = "";
 
 // Declare and initialize room tracking var
-int roomCount = 0;
+int roomCount;
 
 // Ask player for name and get input
 Console.WriteLine("Speak your name stranger...");
@@ -42,6 +43,7 @@ if (playerInput == "warrior")
     playerClass = playerInput;
     playerDMG = warriorDMG;
     playerHP = warriorHP;
+    classBoss = "Azou\'laz, God of the ethereal";
 
     roomCount = 1;
 }
@@ -50,6 +52,7 @@ else if (playerInput == "wizard")
     playerClass = playerInput;
     playerDMG = wizardDMG;
     playerHP = wizardHP;
+    classBoss = "Hermak'ul, God of fury";
 
     roomCount = 1;
 }
@@ -59,7 +62,7 @@ else if (playerInput == "wretch")
     playerDMG = wretchDMG;
     playerHP = wretchHP;
     playerCanFlee = true;
-
+    classBoss = "Ul'yaakir, God of puppets";
     roomCount = 1;
 }
 else
@@ -69,26 +72,28 @@ else
 }
 
 // First room/scene
-if (roomCount == 1 && playerSelectionInvalid == false)
+if (roomCount == 1)
 {
     // Give opponent hp value, damage value, and name, inform player of playername and class choice
     opponentHP = 60;
     opponentDMG = 25;
     opponentName = "ghoul";
-    
-    Console.WriteLine($"{playerName}, the {playerClass}...\ntype anything to continue...");
+
+    Console.Clear();
+    Console.WriteLine($"{playerName}, the {playerClass}...\npress ENTER to continue...");
     Console.ReadLine();
     Console.Clear();
 
     // Print game intro, along with first attack sequence
-    Console.WriteLine("TODO PRINT INTRO to dungeon of cupidity");
-    Console.WriteLine("A ghoul blocks the way, what will you do: attack, flee, or sacrifice?");
+    Console.WriteLine($"{playerName} the {playerClass}, after a long and arduous journey, has found themselves at the entrance of the Dungeon of Cupidity...");
+    Console.WriteLine($"A {opponentName} blocks the way, what will you do? \"attack\", \"flee\", or \"sacrifice?\"");
     playerInput = Console.ReadLine();
 
     // if player attacks, inform player of relevant information
     // If opponent is still alive, opponent attacks, followed by player attacking
     if (playerInput == "attack")
     {
+        Console.Clear();
         opponentHP -= playerDMG;
         Console.WriteLine($"{playerName} the {playerClass} attacks the {opponentName}, dealing {playerDMG} damage!");
         if (opponentHP > 0)
@@ -98,7 +103,7 @@ if (roomCount == 1 && playerSelectionInvalid == false)
             Console.WriteLine($"\n{opponentName} strikes, dealing {opponentDMG} damage!!!");
             opponentHP -= playerDMG;
             Console.WriteLine($"{playerName} the {playerClass} attacks the {opponentName}, dealing {playerDMG} damage!");
-            if ( opponentHP <= 0)
+            if (opponentHP <= 0)
             {
                 roomCount = 2;
                 playerGold += 25;
@@ -111,6 +116,7 @@ if (roomCount == 1 && playerSelectionInvalid == false)
         else
         {
             // This line is for wretches, as their damage one shots enemies
+            Console.Clear();
             if (opponentHP <= 0)
             {
                 roomCount = 2;
@@ -126,6 +132,7 @@ if (roomCount == 1 && playerSelectionInvalid == false)
     // Else if player cannot flee, inform them and fail (no loops!!)
     else if (playerInput == "flee")
     {
+        Console.Clear();
         if (playerCanFlee)
         {
             Console.WriteLine("You successfully flee the room, the ghoul is no longer concerned with you...");
@@ -135,6 +142,7 @@ if (roomCount == 1 && playerSelectionInvalid == false)
         else
         {
             Console.WriteLine($"{playerClass}'s can not flee.");
+            Console.WriteLine();
             roomCount = 666;
         }
     }
@@ -151,6 +159,7 @@ if (roomCount == 1 && playerSelectionInvalid == false)
     }
 }
 
+// Hallway room with chest to the right, and store to the left
 if (roomCount == 2)
 {
     Console.Clear();
@@ -184,36 +193,37 @@ if (roomCount == 3)
     playerInput = Console.ReadLine();
     if (playerInput == "yes")
     {
+        Console.Clear();
         playerGold += 100;
         playerHP += 75;
         roomCount = 4;
         Console.WriteLine("You feel refreshed... and your pockets feel heavier!");
         Console.WriteLine($"{playerName} now has {playerHP}hp and {playerGold} gold!");
-        Console.WriteLine($"{playerName} heads back through the hole in the wall\ntype anything to continue");
+        Console.WriteLine($"{playerName} heads back through the hole in the wall\npress ENTER to continue...");
         Console.ReadLine();
     }
     else
     {
         roomCount = 4;
-        Console.WriteLine($"{playerName} the {playerClass} takes no risks... \ntype anything to head back");
+        Console.WriteLine($"{playerName} the {playerClass} takes no risks... \n{playerName} heads back through the hole in the wall\npress ENTER to continue...");
         Console.ReadLine();
     }
 }
 
-// Player chose the door to their left in the last room / found the chest in room 3
+// Player chose the door to their left  in the last room or left the room to their right
 // This is the store room
 // Player requires the sword to beat boss
 if (roomCount == 4)
 {
     Console.Clear();
     Console.WriteLine($"{playerName} stumble upon an old blind man, he begs you trade him 120 gold for his sword...");
-    Console.WriteLine($"you currently have {playerGold} gold\n you can attempt to haggle only once");
+    Console.WriteLine($"you currently have {playerGold} gold\nyou can attempt to haggle only once");
     Console.WriteLine("\ntype the amount you would like to haggle for. \"100\", for example");
     playerInput = Console.ReadLine();
 
     if (int.Parse(playerInput) >= 90 && playerGold >= 90)
     {
-        Console.WriteLine("You have acquired: \"Blind Mans Folley, Greatsword\"");
+        Console.WriteLine($"{playerName} has acquired: \"Blind Mans Folley, Greatsword\"");
         playerDMG = 1337;
         roomCount = 5;
     }
@@ -222,31 +232,96 @@ if (roomCount == 4)
         Console.WriteLine("The old man suffers a heart attack, and upon falling, shatters the brittle sword");
         roomCount = 5;
     }
+    Console.WriteLine("press ENTER to continue...");
+    Console.ReadLine();
 }
 
+// Final room, contains a boss
 if (roomCount == 5)
 {
+    Console.Clear();
+    opponentDMG = 100;
+    opponentHP = 1000;
 
+    Console.WriteLine($"{playerName} the {playerClass} discovers {classBoss}... The door shuts behind them.");
+    Console.WriteLine($"{classBoss} blocks the way, what will you do? \"attack\", or \"sacrifice?\"");
+    playerInput = Console.ReadLine();
+
+    // if player attacks, inform player of relevant information
+    // If opponent is still alive, opponent attacks, followed by player attacking
+    if (playerInput == "attack")
+    {
+        Console.Clear();
+        opponentHP -= playerDMG;
+        Console.WriteLine($"{playerName} the {playerClass} attacks the {classBoss}, dealing {playerDMG} damage!");
+
+        if (opponentHP > 0)
+        {
+            Console.WriteLine($"{classBoss} has {opponentHP}hp left!");
+            playerHP -= opponentDMG;
+            Console.WriteLine($"\n{classBoss} strikes, dealing {opponentDMG} damage!!!");
+            if (playerHP > 0)
+            {
+                opponentHP -= playerDMG;
+                Console.WriteLine($"\n{playerName} the {playerClass} attacks the {classBoss}, dealing {playerDMG} damage!");
+                if (opponentHP <= 0)
+                {
+                    playerGold += 250;
+                    Console.WriteLine($"\n{playerName} the {playerClass} defeated {classBoss}!!");
+                    Console.WriteLine($"\n{playerName} now has {playerGold} gold, and {playerHP}hp!");
+                    Console.WriteLine($"{playerName} is free to leave the dungeon...");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{classBoss} has delt a fatal blow...\n{playerName} the {playerClass} has been vanquished");
+            }
+        }
+        else
+        {
+            // This line is for when opponent has been one-shot
+            Console.Clear();
+            if (opponentHP <= 0)
+            {
+                playerGold += 250;
+                Console.WriteLine($"{playerName} the {playerClass} defeated the {classBoss}!!");
+                Console.WriteLine($"\n{playerName} now has {playerGold} gold, and {playerHP}hp...");
+                Console.WriteLine($"{playerName} is free to leave the dungeon...");
+            }
+        }
+    }
+    // If player gives up, player response is thus valid, prompt different text based off class
+    else if (playerInput == "sacrifice")
+    {
+        roomCount = 666;
+    }
+    // If player input is invalid, end the game
+    else
+    {
+        roomCount = 666;
+        playerSelectionInvalid = true;
+    }
 }
 
-// Failure room, ends game
+// Failure room, can be directed to from any other room, ends game
 if (roomCount == 666)
 {
     Console.Clear();
-    if(playerSelectionInvalid)
+    if (playerSelectionInvalid)
     {
         Console.WriteLine("As you stumble your words, you fall dead on the spot");
-        Console.ReadLine();
     }
     else
     {
+        // Wretch's follow no god, and no god will lay claim to their soul
+        // Warriors and Wizards, when sacrificing themselves, do not have their soul claimed by their own god.
         if (playerClass == "wretch")
         {
             Console.WriteLine("You sacrifice your life for no god...");
         }
         else
         {
-            Console.WriteLine("You sacrifice your life for your god...");
+            Console.WriteLine($"You sacrifice your life, but your god is not pleased...\n{classBoss} claims your soul instead...");
         }
     }
 }
